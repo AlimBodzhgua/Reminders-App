93c9c06336fce3579417ef0e62d2b04b2d7b4df8
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-05-22 21:33:42 +0300

    checkAuth renamed to requireAuth

--- server/src/routes/reminders.ts
+++ server/src/routes/reminders.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import { reminderCreateValidation, reminderUpdateValidation } from '../validations/validations';
 import * as ReminderContoller from '../controllers/ReminderContoller';
-import checkAuth from '../utils/checkAuth';
+import requireAuth from '../utils/requireAuth';
 
 const router = Router();
 
 // /reminders
-router.post('/', checkAuth, reminderCreateValidation, ReminderContoller.create);
-router.get('/', checkAuth, ReminderContoller.getAll);
-router.get('/:id', checkAuth, ReminderContoller.getOne);
-router.delete('/:id', checkAuth, ReminderContoller.remove);
-router.patch('/:id', checkAuth, reminderUpdateValidation, ReminderContoller.update);
+router.post('/', requireAuth, reminderCreateValidation, ReminderContoller.create);
+router.get('/', requireAuth, ReminderContoller.getAll);
+router.get('/:id', requireAuth, ReminderContoller.getOne);
+router.delete('/:id', requireAuth, ReminderContoller.remove);
+router.patch('/:id', requireAuth, reminderUpdateValidation, ReminderContoller.update);
 
 export default router;
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
 import { loginValidation, registerValidation } from '../validations/validations';
-import checkAuth from '../utils/checkAuth';
+import requireAuth from '../utils/requireAuth';
 import * as UserController from '../controllers/UserController';
 
 const router = Router();
@@ -8,7 +8,7 @@ const router = Router();
 router.get('/', async (req: Request, res: Response) => {
 	return res.send({'message': 'user main page'});
 })
-router.get('/auth/me', checkAuth, UserController.getMe);
+router.get('/auth/me', requireAuth, UserController.getMe);
 router.post('/auth/login', loginValidation, UserController.login);
 router.post('/auth/register', registerValidation, UserController.register);
 

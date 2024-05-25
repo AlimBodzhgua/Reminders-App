33521b797ec7dacf92ba8711504078fff346a190
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-05-25 18:59:37 +0300

    added type checker plugin, removed eslint plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
 	plugins: [
 		react(),
 		tsconfigPaths(),
-		//eslint()
+		checker({
+			typescript: true
+		})
 	],
 });

c7bb2e9d365fbbf4b4bfc3b0f00d989bc33b23b8
AlimBodzhgua <bodzhgua2015@mail.ru>
2024-06-12 22:11:37 +0300

    added spin loader for loading state

--- client/src/components/Layout/Content/Content.tsx
+++ client/src/components/Layout/Content/Content.tsx
@@ -1,16 +1,17 @@
 import { FC, memo, useState } from 'react';
-import { Button, Flex, Space } from 'antd';
+import { Button, Flex, Space, Spin } from 'antd';
 import { useAppSelector } from 'hooks/redux';
 import { selectActiveList } from 'store/selectors/activeListSelectors';
 import { AddReminderForm } from 'components/AddReminderForm/AddReminderForm';
 import { RemindersList } from 'components/RemindersList';
-import { selectUserAuthData } from 'store/selectors/userSelectors';
+import { selectUserAuthData, selectUserIsLoading } from 'store/selectors/userSelectors';
 import { StyledPlusOutlined, StyledContent, StyledTitle } from './Content.styles';
 
 export const Content: FC = memo(() => {
 	const activeList = useAppSelector(selectActiveList);
 	const [showForm, setShowForm] = useState<boolean>(false);
 	const authData = useAppSelector(selectUserAuthData);
+	const isLoading = useAppSelector(selectUserIsLoading);
 	const showEmptyTitle = !activeList?.reminders.length && !showForm;
 
 	const onShowForm = () => {
@@ -20,6 +21,16 @@ export const Content: FC = memo(() => {
 	const onToggleShowForm = () => {
 		setShowForm(prev => !prev);
 	};
+
+	if (isLoading) {
+		return (
+			<StyledContent>
+				<Flex justify='center' align='center' style={{ height: '80%' }}>
+					<Spin size='large' />
+				</Flex>
+			</StyledContent>
+		)
+	}
 
 	if (!authData) {
 		return (

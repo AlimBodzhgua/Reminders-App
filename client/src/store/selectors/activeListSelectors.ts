d9d3dd9d0138d4e8fc901a0e73e5c6927232ed8f
AlimBodzhgua <bodzhgua2015@mail.ru>
2025-01-28 00:27:47 +0300

    deleted unused selector

--- client/src/store/selectors/activeListSelectors.ts
+++ client/src/store/selectors/activeListSelectors.ts
@@ -1,10 +1,4 @@
 import { AppState } from '../config/AppState';
-import { createSelector } from '@reduxjs/toolkit';
 
 export const selectActiveList = (state: AppState) => state.activeList.list;
 export const selectActiveListType = (state: AppState) => state.activeList.listType;
-
-// export const selectNumberCompletedReminders = createSelector(
-// 	selectActiveList,
-// 	(activeList) => activeList?.reminders?.map((list) => list.isCompleted).length
-// );

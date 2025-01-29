bbc22380afcd85363558b2035157a4dce7298c54
AlimBodzhgua <bodzhgua2015@mail.ru>
2025-01-29 14:27:38 +0300

    added input focus and blur on hotkeys press

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,5 @@
-import { FC, useEffect, memo } from 'react';
-import { Input } from 'antd';
+import { FC, useEffect, memo, useRef, useCallback } from 'react';
+import { Input, Typography } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import { useDebounce } from 'hooks/useDebounce';
 import { useAppSelector, useAppDispatch } from 'hooks/redux';
@@ -8,16 +8,34 @@ import { activeListActions } from 'store/slices/activeListSlice';
 import { selectSearchBarValue } from 'store/selectors/searchBarSelectors';
 import { selectAllReminders } from 'store/selectors/userSelectors';
 import { ACTIVE_LIST_LOCALSTORAGE_KEY } from 'constants/localStorage';
+import type { InputRef } from 'antd';
 
 export const SearchBar: FC = memo(() => {
 	const dispatch = useAppDispatch();
 	const searchValue = useAppSelector(selectSearchBarValue);
 	const debouncedSearchValue = useDebounce(searchValue);
 	const allReminders = useAppSelector(selectAllReminders);
+	const inputRef = useRef<InputRef | null>(null);
 
 	const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
 		dispatch(searchBarActions.setValue(e.target.value));
 	};
+
+	const onHotkeyPress = useCallback((e: KeyboardEvent) => {
+		const isActive = inputRef.current?.input === document.activeElement;
+
+		if (e.altKey && (e.key === 'k' || e.key === 'л')) {
+			isActive ? inputRef.current?.blur() : inputRef.current?.focus();
+		} else if (e.key === 'Escape' && isActive) {
+			inputRef.current?.blur();
+		}
+	}, []);
+
+	useEffect(() => {
+		window.addEventListener('keydown', onHotkeyPress);
+
+		return () => window.removeEventListener('keydown', onHotkeyPress);
+	}, [onHotkeyPress]);
 
 	useEffect(() => {
 		if (debouncedSearchValue.length) {
@@ -36,9 +54,11 @@ export const SearchBar: FC = memo(() => {
 	return (
 		<Input
 			addonBefore={<SearchOutlined />}
-			placeholder='Search'
+			suffix={<Typography.Text keyboard>alt + k</Typography.Text>}
 			value={searchValue}
 			onChange={onSearch}
+			ref={inputRef}
+			placeholder='Search'
 		/>
 	);
 });

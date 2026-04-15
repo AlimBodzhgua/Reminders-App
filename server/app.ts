02e50561bfb187742091e15728484ea720f89fd3
AlimBodzhgua <bodzhgua2015@mail.ru>
2026-04-15 10:10:35 +0300

    deleted hello world route

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,9 @@
-import express, { Response, Request } from 'express';
-import router from './src/routes';
-import errorHandler from './src/middleware/errorHandler';
+import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import 'dotenv/config';
+import router from './src/routes';
+import errorHandler from './src/middleware/errorHandler';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -22,12 +22,6 @@ mongoose
 	.connect(DATABASE)
 	.then(() => console.log('DB Connection successful'))
 	.catch((error) => console.log('DB Connection failed', error))
-
-
-app.get('/', (req: Request, res: Response) => {
-	return res.send({message: 'Hello World'})
-})
-
 
 app.listen(port, () => {
 	console.log(`App is running on http://localhost:${port}/`);
